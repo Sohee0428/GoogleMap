4c5cd0d5d06cee071765e74e6741fdcca271e33a
Sohee <sohee3480@gmail.com>
2021-10-08 17:28:57 +0900

    데이터 대입해주기

--- app/src/main/java/com/example/aoppart4chapter03/SearchRecyclerviewAdapter.kt
+++ app/src/main/java/com/example/aoppart4chapter03/SearchRecyclerviewAdapter.kt
@@ -4,25 +4,24 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.aoppart4chapter03.databinding.ViewholderSearchResultItemBinding
+import com.example.aoppart4chapter03.model.SearchResultEntity
 
-class SearchRecyclerviewAdapter (
-    private val searchResultClickListener: (Any) -> Unit) :
-    RecyclerView.Adapter<SearchRecyclerviewAdapter.SearchResultItemViewHolder>() {
+class SearchRecyclerviewAdapter: RecyclerView.Adapter<SearchRecyclerviewAdapter.SearchResultItemViewHolder>() {
 
-
-    private var searchResultList: List<Any> = listOf()
+    private var searchResultList: List<SearchResultEntity> = listOf()
+    private lateinit var searchResultClickListener: (SearchResultEntity) -> Unit
 
     class SearchResultItemViewHolder(
         val binding: ViewholderSearchResultItemBinding,
-        val searchResultClickListener: (Any) -> Unit
+        val searchResultClickListener: (SearchResultEntity) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bindData(data: Any) = with(binding) {
-            textTxt.text = "제목"
-            subTxt.text = "부제목"
+        fun bindData(data: SearchResultEntity) = with(binding) {
+            textTxt.text = data.name
+            subTxt.text = data.fullAddress
         }
 
-        fun bindViews(data: Any) {
+        fun bindViews(data: SearchResultEntity) {
             binding.root.setOnClickListener {
                 searchResultClickListener(data)
             }
@@ -40,9 +39,15 @@ class SearchRecyclerviewAdapter (
     }
 
     override fun onBindViewHolder(holder: SearchResultItemViewHolder, position: Int) {
-        holder.bindData(Any())
-        holder.bindViews(Any())
+        holder.bindData(searchResultList[position])
+        holder.bindViews(searchResultList[position])
     }
 
-    override fun getItemCount(): Int = 10
+    override fun getItemCount(): Int = searchResultList.size
+
+    fun setSearchResultList(searchResultList: List<SearchResultEntity>, searchResultClickListener: (SearchResultEntity) -> Unit) {
+        this.searchResultList = searchResultList
+        this.searchResultClickListener = searchResultClickListener
+        notifyDataSetChanged()
+    }
 }

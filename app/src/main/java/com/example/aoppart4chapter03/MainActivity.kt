3247acef2b9b4ed5d11d3d266ee8cc127d2cdbf1
Sohee <sohee3480@gmail.com>
2021-10-08 17:36:25 +0900

    토루틴 이용

--- app/src/main/java/com/example/aoppart4chapter03/MainActivity.kt
+++ app/src/main/java/com/example/aoppart4chapter03/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.aoppart4chapter03
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.example.aoppart4chapter03.databinding.ActivityMainBinding
+import com.example.aoppart4chapter03.model.LocationLatLngEntity
+import com.example.aoppart4chapter03.model.SearchResultEntity
+import com.example.aoppart4chapter03.response1.search.Poi
+import com.example.aoppart4chapter03.response1.search.Pois
+import com.example.aoppart4chapter03.utility.RetrofitUtil
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: SearchRecyclerviewAdapter
@@ -16,6 +29,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        job = Job()
 
         initAdapter()
         initViews()
@@ -28,8 +43,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initViews() = with(binding) {
-        emptyResultTxt.isVisible = false
-        recycleView.adapter = adapter
+
+
+    private fun searchKeyword(ketwordString: String) {
+        launch(coroutineContext) {
+            try {
+                withContext(Dispatchers.IO) {
+                    val response = RetrofitUtil.apiService.getSearchLocation(
+                        keyword = ketwordString
+                    )
+                    if (response.isSuccessful) {
+                        val body = response.body()
+                        withContext(Dispatchers.Main) {
+                            Log.e("response", body.toString())
+                            body?.let { searchResponse ->
+                                setData(searchResponse.searchPoiInfo.pois)
+                            }
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
+
 }

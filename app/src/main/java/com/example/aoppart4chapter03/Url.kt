2789dc7c3b758ca673958e3e7e5030c14caa3bbf
Sohee <sohee3480@gmail.com>
2021-10-09 16:08:11 +0900

    현 위치를 알기 위한 api 설정

--- app/src/main/java/com/example/aoppart4chapter03/Url.kt
+++ app/src/main/java/com/example/aoppart4chapter03/Url.kt
@@ -4,4 +4,6 @@ object Url {
     const val TMAP_URL = "https://apis.openapi.sk.com"
 
     const val GET_TMAP_LOCATION = "/tmap/pois"
+
+    const val GET_TAMP_REVERSE_GEO_CODE = "/tmap/geo/reversegeocoding"
 }
--- app/src/main/java/com/example/aoppart4chapter03/utility/ApiService.kt
+++ app/src/main/java/com/example/aoppart4chapter03/utility/ApiService.kt
@@ -1,7 +1,10 @@
 package com.example.aoppart4chapter03.utility
 
 import com.example.aoppart4chapter03.Key
+import com.example.aoppart4chapter03.Url
+import com.example.aoppart4chapter03.Url.GET_TAMP_REVERSE_GEO_CODE
 import com.example.aoppart4chapter03.Url.GET_TMAP_LOCATION
+import com.example.aoppart4chapter03.response.address.AddressInfoResponse
 import com.example.aoppart4chapter03.response.search.SearchResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -17,4 +20,15 @@ interface ApiService {
         @Query("count") count: Int = 20,
         @Query("searchKeyword") keyword: String
     ): Response<SearchResponse>
+
+    @GET(GET_TAMP_REVERSE_GEO_CODE)
+    suspend fun getReverseGeoCode(
+        @Header("appKey") appKey: String = Key.TMAP_API,
+        @Query("version") version: Int = 1,
+        @Query("callback") callback: String? = null,
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("coordType") coordType: String? = null,
+        @Query("addressType") addressType: String? = null
+    ): Response<AddressInfoResponse>
 }
